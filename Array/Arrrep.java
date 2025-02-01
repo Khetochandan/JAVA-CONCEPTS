ef792a85f43c9b3aede8e33498d030d3877c181a
Chandan Kheto <chandankheto46@gmail.com>
2025-02-01 18:30:09 +0530

    Add files via upload

---
+++ Array/Arrrep.java
@@ -0,0 +1,16 @@
+public class Arrrep {
+  public static void main(String[] args) {
+    
+    int[] arr = new int[5];
+    arr[0] = 12;
+    arr[1] = 10;
+    arr[2] = 8;
+    arr[3] = 2;
+    arr[4] = 8;
+    
+    System.out.print("Arr element are: ");
+     for(int i=0; i<5; i++){
+      System.out.print(arr[i] + " ");
+     }
+  }
+}
---
+++ Array/Arrrep1.java
@@ -0,0 +1,15 @@
+public class Arrrep1 {
+  public static void main(String[] args) {
+    
+    int[] arr = {12,10,4,5,7,8};
+    System.out.println("Arr element are: ");
+     
+    for(int i=0; i<=arr.length; i++){
+
+      System.out.println(i + "Element is: " + arr[i] );
+    }
+  
+
+  }
+  
+}
